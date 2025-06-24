ed358ab546d6e74dbfafe58d94d8df93c899bbf7
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-06-24 17:12:57 +0700

    adjust: dir name

---
+++ activity_lifecycle/app/src/main/java/com/example/dessertclicker/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.dessertclicker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colour
+val ChocolateBrown = Color(0xFF6F4E37) // Cokelat (Primary)
+val CreamyWhite = Color(0xFFFFFDD0)    // Krim (On Primary)
+val LightBrown = Color(0xFFD2B48C)     // Cokelat muda (Secondary Container)
+val DarkText = Color(0xFF4B382A)       // Teks gelap (On Secondary Container)
+val BakeryBackground = Color(0xFFF5F5DC) // Background utama
+
+// Dark Theme Colour
+val DarkChocolate = Color(0xFF4A3728)      // Cokelat Gelap (Primary)
+val PaleCream = Color(0xFFFAF0E6)         // Krim Pucat (On Primary)
+val MediumBrown = Color(0xFF8B7355)     // Cokelat medium (Secondary Container)
+val LightText = Color(0xFFFFF5EE)         // Teks terang (On Secondary Container)
+val DarkBackground = Color(0xFF2C1F16)        // Background gelap
